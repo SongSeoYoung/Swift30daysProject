0a4c0400ea390ec6da2403aba1ee7900030a908d
seoyoung <seoyoung_00@hufs.ac.kr>
2020-12-28 17:09:29 +0900

    [add] gitignore

---
+++ Project 01 - GoodAsOldPhones/Project 01 - GoodAsOldPhones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 01 - GoodAsOldPhones
+//
+//  Created by 송서영 on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
