0ffdee9da9bc2d800dde6e37521ab6e8582d6c5a
seoyoung <seoyoung_00@hufs.ac.kr>
2020-12-28 18:21:55 +0900

    [add] 전체 UI 구성

---
+++ Project 01 - GoodAsOldPhones/Project 01 - GoodAsOldPhones/UsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UsViewController.swift
+//  Project 01 - GoodAsOldPhones
+//
+//  Created by 송서영 on 2020/12/28.
+//
+
+import UIKit
+
+class UsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
