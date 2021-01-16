76061b7e542abba2fc5b28a86bbac39a0647f594
seoyoung <seoyoung_00@hufs.ac.kr>
2021-01-16 20:15:34 +0900

    [add] Kingfisher Podfile

--- Project 07 - PokedexGo/Project 07 - PokedexGo/MasterTableView.swift
+++ Project 07 - PokedexGo/Project 07 - PokedexGo/MasterTableView.swift
@@ -6,8 +6,25 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MasterTableView: UITableViewController {
+    
 
-
+    override func viewDidLoad() {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell: MasterTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MasterTableViewCell else { return UITableViewCell() }
+        cell.title.text = pokemons[indexPath.row].name
+        cell.label.text = pokemons[indexPath.row].detailInfo
+        return cell
+    }
 }
+
+
