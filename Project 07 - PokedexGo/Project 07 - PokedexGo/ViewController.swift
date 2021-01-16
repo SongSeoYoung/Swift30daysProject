97b71a548974fc3579e9036b1517bcf45a45b1d5
seoyoung <seoyoung_00@hufs.ac.kr>
2021-01-16 16:43:30 +0900

    [add] poject 7 init

---
+++ Project 07 - PokedexGo/Project 07 - PokedexGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 07 - PokedexGo
+//
+//  Created by 송서영 on 2021/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
