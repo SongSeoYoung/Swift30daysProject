986b0a3cbcde18348d9f70d57c3bc256c2d866cc
seoyoung <seoyoung_00@hufs.ac.kr>
2021-01-18 14:59:51 +0900

    [add] project 8

---
+++ Project 08 - SimpleRSSReader/Project 08 - SimpleRSSReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 08 - SimpleRSSReader
+//
+//  Created by 송서영 on 2021/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
