baacec27747c06e814e2a8199c67e860bafa9518
seoyoung <seoyoung_00@hufs.ac.kr>
2021-01-24 18:43:03 +0900

    [feat] Pinch Image

--- Project 09 - PhotoScroll/Project 09 - PhotoScroll/PhotoDetailViewController.swift
+++ Project 09 - PhotoScroll/Project 09 - PhotoScroll/PhotoDetailViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 class PhotoDetailViewController: UIViewController, UIGestureRecognizerDelegate{
 
     @IBOutlet weak var pageControl: UIPageControl!
-    @IBOutlet weak var PhotoImage: UIImageView!
+    @IBOutlet weak var photoImage: UIButton!
+    
     let PhotoList: Photo = Photo()
     var number: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let indexNumber = self.number else {return }
-        self.PhotoImage.image = PhotoList.photoList[indexNumber]
+        self.photoImage.setImage(PhotoList.photoList[indexNumber], for: .normal)
         pageControl.numberOfPages = PhotoList.photoList.count
         pageControl.currentPage = indexNumber
 
 
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        (segue.destination as? PinchPhotoViewController)?.number = self.number
     }
 
 
--- Project 09 - PhotoScroll/Project 09 - PhotoScroll/PinchPhotoViewController.swift
+++ Project 09 - PhotoScroll/Project 09 - PhotoScroll/PinchPhotoViewController.swift
@@ -13,18 +13,40 @@ class PinchPhotoViewController: UIViewController, UIGestureRecognizerDelegate {
     let photoList: Photo = Photo()
     @IBOutlet var pinchGesture: UIPinchGestureRecognizer!
     var number: Int?
+    var firstHeight: CGFloat?
+    var firstWidth: CGFloat?
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("pinch")
         if let indexNumber = self.number {
+            print(indexNumber)
             pinchPhoto.image = photoList.photoList[indexNumber]
         }
         pinchPhoto.isUserInteractionEnabled = true
 
+        firstHeight = self.pinchPhoto.frame.height
+        firstWidth = self.pinchPhoto.frame.width
     }
+    
+    
     
 
     @IBAction func pinchAction(_ sender: Any) {
+        let newWidth = pinchGesture.scale * self.pinchPhoto.frame.width
+        let newHeight = pinchGesture.scale * self.pinchPhoto.frame.height
+        
+        print(newWidth, newHeight)
+        
+        guard let originWidth = self.firstWidth,
+              let originHeight = self.firstHeight else { return }
+        if (newWidth < originWidth) && (newHeight < originHeight) {
+            print("minumum")
+            pinchPhoto.frame.size = CGSize(width: originWidth, height: originHeight)
+            pinchGesture.scale = 1.0
+        }
         pinchPhoto.transform = pinchPhoto.transform.scaledBy(x: pinchGesture.scale, y: pinchGesture.scale)
         pinchGesture.scale = 1.0
     }
+    
+    
 }
