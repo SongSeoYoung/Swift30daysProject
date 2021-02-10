e309badacda04d39d7598844bc3a3e2052d54560
seoyoung <seoyoung_00@hufs.ac.kr>
2021-02-10 22:08:15 +0900

    [add] Project 13 UI

--- Project 13 - TwitterBird/Project 13 - TwitterBird/ViewController.swift
+++ Project 13 - TwitterBird/Project 13 - TwitterBird/ViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var backgroundLayer: CALayer = CALayer()
+    var twitterLayer: CALayer = CALayer()
+    
+    @IBOutlet weak var backgroundImg: UIImageView!
+    @IBOutlet var rootView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        backgroundImg.layer.addSublayer(backgroundLayer)
+        setLayer()
+        print("view did load")
     }
+    
+    func setLayer() {
+//        backgroundLayer.backgroundColor = UIColor.blue.cgColor
+//        backgroundLayer.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+//
+        twitterLayer.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        twitterLayer.position = rootView.center
+        twitterLayer.contents = UIImage(named: "twitterBird")?.cgImage
+        
+        backgroundImg.layer.mask = twitterLayer
+        
+        rootView.backgroundColor = UIColor.blue
+    }
+    
+    
 
 
 }
