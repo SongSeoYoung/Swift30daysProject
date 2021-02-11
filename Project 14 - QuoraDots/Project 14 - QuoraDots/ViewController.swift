b58c05108153db72b53de0e90e6a2f0d31341a7e
seoyoung <seoyoung_00@hufs.ac.kr>
2021-02-11 15:22:26 +0900

    [add] Project14 Image

---
+++ Project 14 - QuoraDots/Project 14 - QuoraDots/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 14 - QuoraDots
+//
+//  Created by 송서영 on 2021/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
