7abdae5b4cc5a9dcce71d88c59dfd0e08262566f
seoyoung <seoyoung_00@hufs.ac.kr>
2021-02-13 22:33:49 +0900

    [add] Project 15

---
+++ Project 15 - SnapchatMenu/Project 15 - SnapchatMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 15 - SnapchatMenu
+//
+//  Created by 송서영 on 2021/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
