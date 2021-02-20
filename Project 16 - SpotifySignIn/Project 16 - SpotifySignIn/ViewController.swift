4c8e349d0db6c8fc376c04904a7f67f10894d292
seoyoung <seoyoung_00@hufs.ac.kr>
2021-02-20 13:46:57 +0900

    [style] 코드 스타일 수정

--- Project 16 - SpotifySignIn/Project 16 - SpotifySignIn/ViewController.swift
+++ Project 16 - SpotifySignIn/Project 16 - SpotifySignIn/ViewController.swift
@@ -10,25 +10,42 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var logInBtn: UIButton!
-    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet private weak var logInBtn: UIButton!
+    @IBOutlet private weak var signUpBtn: UIButton!
+    @IBOutlet private weak var videoView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setVideoPlay()
+        setUI()
     }
     
-    func setVideoPlay() {
+    private var player: AVPlayer?
+    
+    private func setVideoPlay() {
         guard let path = Bundle.main.path(forResource: "moments", ofType: "mp4") else { return }
-        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        player = AVPlayer(url: URL(fileURLWithPath: path))
         let playerLayer = AVPlayerLayer(player: player)
         playerLayer.frame = self.view.bounds
         playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        self.view.layer.addSublayer(playerLayer)
-        player.seek(to: CMTime.zero)
-        player.play()
+        self.videoView.layer.addSublayer(playerLayer)
+        player?.seek(to: CMTime.zero)
+        player?.play()
+        NotificationCenter.default
+            .addObserver(forName: .AVPlayerItemDidPlayToEndTime,
+                        object: nil,
+                        queue: nil)
+            { [weak self] note in
+            self?.player?.seek(to: CMTime.zero)
+            self?.player?.play()
+        }
     }
-
-
+    
+    private func setUI() {
+        logInBtn.alpha = 0.9
+        signUpBtn.alpha = 0.9
+    }
+    
+    
 }
 
