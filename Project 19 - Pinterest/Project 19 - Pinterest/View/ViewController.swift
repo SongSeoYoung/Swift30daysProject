85867b96ab93ce2bd78a104207cf99339d9843c8
seoyoung <seoyoung_00@hufs.ac.kr>
2021-02-26 21:01:14 +0900

    [feat] dynamic height cell

--- Project 19 - Pinterest/Project 19 - Pinterest/View/ViewController.swift
+++ Project 19 - Pinterest/Project 19 - Pinterest/View/ViewController.swift
@@ -16,17 +16,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         viewModel.decode()
         setFlowLayout()
-
+        
     }
     
     func setFlowLayout() {
         let flowLayout = UICollectionViewFlowLayout()
 //        flowLayout.minimumLineSpacing = 5
-        flowLayout.sectionInset = UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
+        flowLayout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
 //        flowLayout.itemSize = CGSize(width: (UIScreen.main.bounds.width / 2) - 10, height: 300)
 //        flowLayout.estimatedItemSize = CGSize(width: (UIScreen.main.bounds.width / 2) - 10, height: 300)
 //        flowLayout.scrollDirection = .vertical
-        flowLayout.estimatedItemSize = CGSize(width: UIScreen.main.bounds.width / 2 - 10, height: 500)
+//        flowLayout.estimatedItemSize = CGSize(width: UIScreen.main.bounds.width / 2 - 10, height: 500)
         self.mainCollectionView.collectionViewLayout = flowLayout
     }
 
@@ -34,20 +34,15 @@ class ViewController: UIViewController {
 
 
  // MARK: - Viewcontroller DataSource Delegate FlowLayoutDelegate
-extension ViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension ViewController : UICollectionViewDelegate, UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         viewModel.getModelCount()
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "mainCollectionViewCell", for: indexPath) as? mainCollectionViewCell {
-            print("setting size")
-            return cell.fittingSize()
-        }
-        return CGSize(width: 0, height: 0)
-    }
+
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        print("cell for item at")
         guard let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "mainCollectionViewCell", for: indexPath) as? mainCollectionViewCell else { return UICollectionViewCell() }
         cell.captionLabel.text = viewModel.photosModel?[indexPath.row]["Caption"]
         cell.commentLabel.text = viewModel.photosModel?[indexPath.row]["Comment"]
@@ -55,6 +50,7 @@ extension ViewController : UICollectionViewDelegate, UICollectionViewDataSource,
             cell.imageView.image = UIImage(named: photoNumber)
         }
         cell.layer.cornerRadius = 12.0
+        cell.backgroundColor = .red
         return cell
     }
 
@@ -71,5 +67,34 @@ extension ViewController: PinterestLayoutDelegate {
             return photo?.size.height ?? 0
         }
         return 0
+    }
+}
+
+extension ViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        print("size for item at")
+        guard let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "mainCollectionViewCell", for: indexPath) as? mainCollectionViewCell else { return CGSize(width: 0, height: 0)}
+        let photo: UIImage?
+        let photoHeight: CGFloat?
+        guard let photoNumber = viewModel.photosModel?[indexPath.row]["Photo"] else {
+            return CGSize(width: 0, height: 0)
+        }
+        photo = UIImage(named: photoNumber)
+        photoHeight = photo?.size.height ?? 0
+        let contentHeight = cell.captionLabel.bounds.height + cell.commentLabel.bounds.height
+        cell.captionLabel.text = viewModel.photosModel?[indexPath.row]["Caption"]
+        cell.commentLabel.text = viewModel.photosModel?[indexPath.row]["Comment"]
+        if let photoNumber = viewModel.photosModel?[indexPath.row]["Photo"] {
+            cell.imageView.image = UIImage(named: photoNumber)
+        }
+        return cell.photoBackView.systemLayoutSizeFitting(CGSize(width: UIScreen.main.bounds.width / 2 - 10, height: UIView.layoutFittingCompressedSize.height), withHorizontalFittingPriority: .required, verticalFittingPriority: .fittingSizeLevel)
+//        print("image view height", photoHeight, "photoBackviewHeight", cell.photoBackView.bounds.height)
+//        return CGSize(width: UIScreen.main.bounds.width / 2 - 10, height: cell.photoBackView.bounds.height)
     }
 }
